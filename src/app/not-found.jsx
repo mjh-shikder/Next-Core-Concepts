afbb27579d0fba7e2a237ba58cc7f65b6cefa38a
mjh-shikder <mjhsikder@gmail.com>
2026-02-17 15:44:42 +0600

    Not found page added

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='h-screen flex justify-center items-center font-bold text-5xl '>
+            <h1>Page Not Found 😘</h1>
+        </div>
+    );
+};
+
+export default NotFound;
