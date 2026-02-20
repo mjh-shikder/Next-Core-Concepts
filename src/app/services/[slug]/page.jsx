799d7b12db2596918da2d3f55e40ace6ecc91435
mjh-shikder <mjhsikder@gmail.com>
2026-02-21 01:10:44 +0600

    Dyanmic route added

---
+++ src/app/services/[slug]/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ServiceDetailsPage = async ({ params }) => {
+    
+    const {} = await params
+
+    return (
+        <div>
+            Service Details Page
+        </div>
+    );
+};
+
+export default ServiceDetailsPage;
---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicesPage = () => {
+    return (
+        <div>
+            Service page
+        </div>
+    );
+};
+
+export default ServicesPage;
