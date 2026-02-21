b59c6707a500bf0a2fa131dd551488eb9fff8df8
mjh-shikder <mjhsikder@gmail.com>
2026-02-22 03:12:08 +0600

    Allow all image source in next.config.js file

--- src/components/shared/DashboardSidebar.jsx
+++ src/components/shared/DashboardSidebar.jsx
@@ -1,10 +1,16 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const DashboardSidebar = () => {
     return (
         <div className='w-[200px] bg-purple-200 text-gray-800 h-screen fixed top-0 left-0'>
-            <h2 className='font-bold text-center'>Car Cleanify</h2>
+
+            <Image src={'https://static.vecteezy.com/system/resources/previews/010/486/954/original/blue-car-wash-auto-detailing-logo-vector.jpg'}
+                alt='logo'
+                width={80}
+                height={60}
+            ></Image>
 
             <div className="flex flex-col gap-5 mt-7 px-3 py-5">
                 <Link href={'/dashboard/profile'}>Profile</Link>
